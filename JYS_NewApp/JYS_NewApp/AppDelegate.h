7213d8d101472a1e8805b9ae72eba6811159ceaa
Jianying Sun <jianying.sun@netviewtech.com>
2015-11-20 15:13:46 +0800

    start App

---
+++ JYS_NewApp/JYS_NewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYS_NewApp
+//
+//  Created by Jianying Sun on 15/11/20.
+//  Copyright © 2015年 Netviewtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYS_NewApp/JYS_NewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYS_NewApp
+//
+//  Created by Jianying Sun on 15/11/20.
+//  Copyright © 2015年 Netviewtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
