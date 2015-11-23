8df7f30de80604a49a14eb1f0dac88be8cf97d5e
Jianying Sun <jianying.sun@netviewtech.com>
2015-11-23 12:08:03 +0800

    Add Action

--- JYS_NewApp/JYS_NewApp/JYS_MyDeviceViewController.m
+++ JYS_NewApp/JYS_NewApp/JYS_MyDeviceViewController.m
@@ -18,17 +18,30 @@
     [super viewDidLoad];
     self.navigationItem.titleView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"logo_ios7"]];
     self.view.backgroundColor = [UIColor whiteColor];
-    UIBarButtonItem *rightBarItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"add"] style:UIBarButtonItemStylePlain target:self action:@selector(helpPage)];
-    
+    UIBarButtonItem *rightBarItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"add"] style:UIBarButtonItemStylePlain target:self action:@selector(helpPage:)];
+    rightBarItem.tintColor = self.view.tintColor ;
+    rightBarItem.tag = 1;
     self.navigationItem.rightBarButtonItem = rightBarItem;
 
-    UIBarButtonItem *eftBarItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"card@1x"] style:UIBarButtonItemStylePlain target:self action:@selector(helpPage)];
-    
-    self.navigationItem.leftBarButtonItem = eftBarItem;
+    UIBarButtonItem *eftBarItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"card"] style:UIBarButtonItemStylePlain target:self action:@selector(helpPage:)];
+    eftBarItem.tintColor = self.view.tintColor ;
+    eftBarItem.tag = 0;
 
+    self.navigationItem.leftBarButtonItem = eftBarItem;
 
 }
 
 
+- (void)helpPage:(UIBarButtonItem *)sender
+{
+    if (sender.tag == 0) {
+        NSLog(@"更多");
+
+    }else{
+    
+        NSLog(@"添加");
+
+    }
+}
 
 @end
